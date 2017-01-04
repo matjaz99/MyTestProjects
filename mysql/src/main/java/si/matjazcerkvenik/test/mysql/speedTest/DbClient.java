039784fe4e63c662a8a4862a36d067e72f334644
matjaz <matjaz99@gmail.com>
2017-01-04 01:51:29 +0100

    added mysql speed test (insert only)

---
+++ mysql/src/main/java/si/matjazcerkvenik/test/mysql/speedTest/DbClient.java
@@ -0,0 +1,109 @@
+package si.matjazcerkvenik.test.mysql.speedTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbClient {
+	
+	private Connection c = null;
+	
+	private String host = "192.168.1.106:3306";
+	private String tableName = "nodes";
+	private String username = "mysql";
+	private String password = "";
+	
+	public static int i = 0;
+	
+	public static void main(String[] args) {
+		
+		DbClient dbc = new DbClient();
+		dbc.loadDriver();
+//		dbc.createTable();
+		
+		long startTime = System.currentTimeMillis();
+		
+		int maxNum = 100000;
+		
+		while (i < maxNum) {
+			Node n = new Node(i, "Node #" + i, i, "EWSD", "nodehost-" + i);
+			dbc.insertNode(n);
+			i++;
+		}
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Inserted " + maxNum + " rows in " + (endTime-startTime) + " ms [" + (maxNum * 1000 / (endTime-startTime)) + "/s]");
+		
+		dbc.disconnect();
+		
+	}
+	
+	
+	public void loadDriver() {
+
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			System.out.println("driver loaded");
+			c = DriverManager.getConnection(
+					"jdbc:mysql://" + host + "/test", username, password);
+			System.out.println("connected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public void createTable() {
+
+		String sql = "CREATE TABLE " + tableName
+				+ " (id INT, name VARCHAR(16), nodeId INT, productId VARCHAR(16), hostname VARCHAR(32))";
+
+		try {
+			c.createStatement().execute(sql);
+			System.out.println("createTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void insertNode(Node n) {
+
+		try {
+			PreparedStatement ps = c
+					.prepareStatement("insert into " + tableName + " values (?, ?, ?, ?, ?)");
+			ps.setInt(1, n.getId());
+			ps.setString(2, n.getName());
+			ps.setInt(3, n.getNodeId());
+			ps.setString(4, n.getProductId());
+			ps.setString(5, n.getHostname());
+			ps.executeUpdate();
+			
+			if (i % 100 == 0) {
+				System.out.println("insert: " + n.toString());
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
+	public void disconnect() {
+		if (c == null) {
+			return;
+		}
+		try {
+			c.close();
+			System.out.println("disconnected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ mysql/src/main/java/si/matjazcerkvenik/test/mysql/speedTest/Node.java
@@ -0,0 +1,57 @@
+package si.matjazcerkvenik.test.mysql.speedTest;
+
+public class Node {
+	
+	private int id;
+	private String name;
+	private int nodeId;
+	private String productId;
+	private String hostname;
+	
+	public Node(int id, String name, int nodeId, String productId,
+			String hostname) {
+		this.id = id;
+		this.name = name;
+		this.nodeId = nodeId;
+		this.productId = productId;
+		this.hostname = hostname;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNodeId() {
+		return nodeId;
+	}
+	public void setNodeId(int nodeId) {
+		this.nodeId = nodeId;
+	}
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	@Override
+	public String toString() {
+		return "Node [id=" + id + ", name=" + name + ", nodeId=" + nodeId
+				+ ", productId=" + productId + ", hostname=" + hostname + "]";
+	}
+	
+	
+	
+}
