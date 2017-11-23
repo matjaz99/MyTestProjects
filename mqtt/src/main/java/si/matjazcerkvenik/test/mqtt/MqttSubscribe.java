d2ec50a73e70ff5bcab07353309e7173c9d83660
matjaz <matjaz@192.168.1.201>
2017-11-24 00:32:43 +0100

    MQTT example

---
+++ mqtt/src/main/java/si/matjazcerkvenik/test/mqtt/MqttSubscribe.java
@@ -0,0 +1,156 @@
+package si.matjazcerkvenik.test.mqtt;
+
+import java.util.Random;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+
+public class MqttSubscribe implements MqttCallback {
+	
+	MqttClient myClient;
+	MqttConnectOptions connOpt;
+
+	static final String BROKER_URL = "tcp://localhost:1883";
+	static final String CLIENT_ID = "JavaMqttClient";
+	static final String TOPIC = "mytopic";
+	static final String USERNAME = "username";
+	static final String PASSWORD_MD5 = "password (MD5 sum of password)>";
+
+	// the following two flags control whether this example is a publisher, a subscriber or both
+	static final Boolean subscriber = true;
+	static final Boolean publisher = true;
+
+	/**
+	 * 
+	 * connectionLost
+	 * This callback is invoked upon losing the MQTT connection.
+	 * 
+	 */
+	@Override
+	public void connectionLost(Throwable t) {
+		System.out.println("Connection lost!");
+	}
+
+	/**
+	 * 
+	 * deliveryComplete
+	 * This callback is invoked when a message published by this client
+	 * is successfully received by the broker.
+	 * 
+	 */
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		try {
+			System.out.println("Pub complete: " + new String(token.getMessage().getPayload()));
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * messageArrived
+	 * This callback is invoked when a message is received on a subscribed topic.
+	 * 
+	 */
+	@Override
+	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		System.out.println("==> Message arrived: Topic: " + topic + " Message: " + new String(message.getPayload()));
+	}
+	
+	
+
+	/**
+	 * 
+	 * MAIN
+	 * 
+	 */
+	public static void main(String[] args) {
+		MqttSubscribe ms = new MqttSubscribe();
+		ms.runClient();
+	}
+	
+	/**
+	 * 
+	 * runClient
+	 * The main functionality of this simple example.
+	 * Create a MQTT client, connect to broker, pub/sub, disconnect.
+	 * 
+	 */
+	public void runClient() {
+		// setup MQTT Client
+		connOpt = new MqttConnectOptions();
+		
+		connOpt.setCleanSession(true);
+		connOpt.setKeepAliveInterval(30);
+		connOpt.setUserName(USERNAME);
+		connOpt.setPassword(PASSWORD_MD5.toCharArray());
+		
+		// Connect to Broker
+		try {
+			myClient = new MqttClient(BROKER_URL, CLIENT_ID);
+			myClient.setCallback(this);
+			myClient.connect(connOpt);
+		} catch (MqttException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		System.out.println("Connected to " + BROKER_URL);
+
+		// setup topic
+		MqttTopic topic = myClient.getTopic(TOPIC);
+
+		// subscribe to topic if subscriber
+		if (subscriber) {
+			try {
+				int subQoS = 0;
+				myClient.subscribe(TOPIC, subQoS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		// publish messages if publisher
+		if (publisher) {
+			for (int i = 1; i <= 1000000; i++) {
+		   		String pubMsg = "{\"msg\":" + i + "}";
+		   		int pubQoS = 0;
+				MqttMessage message = new MqttMessage(pubMsg.getBytes());
+		    	message.setQos(pubQoS);
+		    	message.setRetained(false);
+
+		    	// Publish the message
+		    	System.out.println("Publishing to topic " + topic + " qos " + pubQoS);
+		    	MqttDeliveryToken token = null;
+		    	try {
+		    		// publish message to broker
+					token = topic.publish(message);
+			    	// Wait until the message has been delivered to the broker
+					token.waitForCompletion();
+					Thread.sleep(new Random().nextInt(10));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}			
+		}
+		
+		// disconnect
+		try {
+			// wait to ensure subscribed messages are delivered
+			if (subscriber) {
+				Thread.sleep(300 * 1000);
+			}
+			myClient.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ mqtt/src/main/java/si/matjazcerkvenik/test/mqtt/StartMqtt.java
@@ -0,0 +1,5 @@
+package si.matjazcerkvenik.test.mqtt;
+
+public class StartMqtt {
+
+}
