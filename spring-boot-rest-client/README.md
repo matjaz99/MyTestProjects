76b19b41be8a584bcf6bb31306138d443f7a9364
matjaz <matjaz@192.168.1.205>
2018-03-13 01:52:22 +0100

    added exception handling in client

--- spring-boot-rest-client/README.md
+++ spring-boot-rest-client/README.md
@@ -1,6 +1,29 @@
 # Spring boot rest client-server example - Client side
 
-[see also](../spring-boot-rest-impl/README.md)
+This example shows how to write a REST client and put a web gui on top of it.
+REST client is implemented in Spring Boot and Thymeleaf is used for GUI.
+
+Based on examples:
+
+http://g00glen00b.be/producing-rest-apis-with-spring/
+https://g00glen00b.be/consuming-rest-apis-with-spring/
+
+For this example to work, you also need to start the REST server with all the business logic and DB handling:
+[Here](../spring-boot-rest-impl/README.md)
+
+
+Since the REST server is already running on port 8080, the web server of gui should be set to 8081 to avoid conflicting ports.
+
+
+Server (REST):
+
+http://localhost:8080/api/tasks
+
+Client server (GUI):
+
+http://localhost:8081/
+
+
 
 
 
--- spring-boot-rest-client/src/main/java/si/matjazcerkvenik/test/springboot/tasks/TaskController.java
+++ spring-boot-rest-client/src/main/java/si/matjazcerkvenik/test/springboot/tasks/TaskController.java
@@ -1,14 +1,20 @@
 package si.matjazcerkvenik.test.springboot.tasks;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/")
@@ -41,5 +47,16 @@ public class TaskController {
         service.create(task);
         return "redirect:/";
     }
+    
+    
+    @Autowired
+    private ObjectMapper mapper;
 	
+    @ExceptionHandler(HttpClientErrorException.class)
+    public String handleClientError(HttpClientErrorException e, Model model) throws IOException {
+		MessageDTO dto = mapper.readValue(e.getResponseBodyAsByteArray(), MessageDTO.class);
+		model.addAttribute("error", dto.getMessage());
+		return findAll(model);
+	}
+    
 }
