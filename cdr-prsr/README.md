020b69fe6ff3758dc368dbc57283ecc06d9a022a
matjaz99 <matjaz99@gmail.com>
2020-03-04 22:10:15 +0100

    update

---
+++ cdr-prsr/README.md
@@ -0,0 +1,3 @@
+# Title
+
+$ mvn install:install-file -Dfile=cdr-parser-2.0.37.jar -DgroupId=si.iskratel -DartifactId=cdr-parser -Dversion=2.0.37 -Dpackaging=jar
--- cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
+++ cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
@@ -1,12 +1,11 @@
 package si.iskratel.cdr;
 
-import si.iskratel.cdr.parser.CdrParser;
 
 public class Test {
 
     public static void main(String[] args) {
 
-//        CdrParser.parseCDR()
+
 
     }
 
