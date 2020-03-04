75e2c9bd9146e0e34f191a19b135b797846b8602
matjaz <cerkvenik@iskratel.si>
2020-03-04 16:41:00 +0100

    cdr parser test

---
+++ cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
@@ -0,0 +1,13 @@
+package si.iskratel.cdr;
+
+import si.iskratel.cdr.parser.CdrParser;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+//        CdrParser.parseCDR()
+
+    }
+
+}
