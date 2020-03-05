9e1a9495b4f91ffa34dd40032f9662af346ed96d
matjaz <cerkvenik@iskratel.si>
2020-03-05 17:51:25 +0100

    cdr parser test

--- cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
+++ cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
@@ -1,11 +1,21 @@
 package si.iskratel.cdr;
 
 
+import si.iskratel.cdr.parser.CDRReader;
+import si.iskratel.cdr.parser.CdrObject;
+
+import java.io.File;
+import java.util.List;
+
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        List<byte[]> bytes = CDRReader.readCDR(new File("C:\\Users\\cerkvenik\\Desktop\\5cdrs"));
 
+        System.out.println("size: " + bytes.size());
+
+        CdrObject obj = new CdrObject();
 
     }
 
