61f061b5f78b96b47d0aa917665bedfca18d7bcd
matjaz99 <matjaz99@gmail.com>
2020-03-06 07:39:02 +0100

    io

--- cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
+++ cdr-prsr/src/main/java/si/iskratel/cdr/Test.java
@@ -1,6 +1,7 @@
 package si.iskratel.cdr;
 
 
+import org.apache.commons.io.IOUtils;
 import si.iskratel.cdr.parser.CDRReader;
 import si.iskratel.cdr.parser.CdrObject;
 import si.iskratel.cdr.parser.DataRecord;
@@ -17,16 +18,20 @@ public class Test {
         // List<byte[]> bytes = CDRReader.readCDR(new File("C:\\Users\\cerkvenik\\Desktop\\5cdrs"));
         // System.out.println("size: " + bytes.size());
 
-        //File f = new File("/Users/matjaz/Dropbox/Iskratel/PMON/CDR/5cdrs");
+        File f = new File("/Users/matjaz/Dropbox/Iskratel/PMON/CDR/5cdrs");
         //File f = new File("/Users/matjaz/Dropbox/Iskratel/PMON/CDR/111520200205090084");
-        File f = new File("/Users/matjaz/Dropbox/Iskratel/PMON/CDR/2063820200224090086.si2");
+//        File f = new File("/Users/matjaz/Dropbox/Iskratel/PMON/CDR/2063820200224090086.si2");
         FileInputStream is = new FileInputStream(f);
-        ByteArrayInputStream bais = new ByteArrayInputStream(is.readAllBytes());
+//        ByteArrayInputStream bais = new ByteArrayInputStream(is.readAllBytes()); // Java 9!!!
+        byte[] bytes = IOUtils.toByteArray(is);
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
         List<DataRecord> list = CDRReader.readDataRecords(bais);
-        System.out.println("size: " + list.size());
+
         for (DataRecord dr : list) {
             System.out.println(dr.toString());
         }
+
+        System.out.println("size: " + list.size());
 
     }
 
