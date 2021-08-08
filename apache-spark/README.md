f742714cf6defdaad453bb55bdb1c6bd90948307
matjaz99 <matjaz99@gmail.com>
2021-08-08 19:40:58 +0200

    kafka example - counting alerts by severity

---
+++ apache-spark/README.md
@@ -0,0 +1,33 @@
+# Apache Spark
+
+
+http://192.168.0.16:4040/
+
+
+
+
+## Functions
+
+Example: count words in line
+
+Input line:
+
+aa bb aa
+
+flatMap:
+
+aa  
+bb  
+bb
+
+mapToPair:
+
+aa 1  
+bb 1  
+aa 1
+
+reduceByKey:
+
+aa 2  
+bb 1
+
--- apache-spark/src/main/java/si/matjazcerkvenik/test/spark/KafkaStreamAlarmCount.java
+++ apache-spark/src/main/java/si/matjazcerkvenik/test/spark/KafkaStreamAlarmCount.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
@@ -19,17 +20,17 @@ import java.util.regex.Pattern;
 
 public class KafkaStreamAlarmCount {
 
-    private static final Pattern SPACE = Pattern.compile(" ");
+    private static final Pattern SEPARATOR = Pattern.compile(",");
 
     public static void main(String[] args) throws Exception {
 
-        String brokers = "mcrk-docker-1:9092"; // a list of one or more Kafka brokers
+        String brokers = "promvm:9092"; // a list of one or more Kafka brokers
         String groupId = "my_consumer"; // a consumer group name to consume from topics
         String topics = "alertmonitor_notifications"; // a list of one or more kafka topics to consume from
 
         // Create context with a 2 seconds batch interval
         SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("KafkaStreamAlarmCount");
-        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(30));
 
         Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
         Map<String, Object> kafkaParams = new HashMap<>();
@@ -42,9 +43,13 @@ public class KafkaStreamAlarmCount {
         JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils
                 .createDirectStream(jssc, LocationStrategies.PreferConsistent(), ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
 
-        // Get the lines, split them into words, count the words and print
+        // Get the lines
         JavaDStream<String> lines = messages.map(ConsumerRecord::value);
-        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        lines.print();
+        // split them into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SEPARATOR.split(x)).iterator());
+        words.print();
+        // count the words and print
         JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
         wordCounts.print();
 
